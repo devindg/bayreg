Fix to prior adjustment when data are standardized - If a custom prior covariance matrix is provided, it is no longer scaled by the variance of the response variable. This is duplicative because the variance of the coefficients is conditional on the variance of the error term, which reflects the scale of the response

bayreg/bayvar.py
@@ -456,11 +456,6 @@ class BayesianVAR:
 
             prior_coeff_cov = []
             for k in range(num_endog):
-                if standardize_data:
-                    var_y = np.var(endog[:, k])
-                else:
-                    var_y = 1.
-
                 zell_g_k = np.ones(num_pred_vars) * zellner_g
                 mask = np.array([False] * num_pred_vars)
                 mask[:num_lag_vars][k::num_endog] = True
@@ -470,8 +465,7 @@ class BayesianVAR:
                         np.diag(zell_g_k ** 0.5)
                         @ (1 / zellner_g * pcc)
                         @ np.diag(zell_g_k ** 0.5)
-                        @ W) * var_y
-                                       )
+                        @ W))
 
                 if cross_lag_endog_zellner_g_factor < 1:
                     pcm = np.asarray(prior_coeff_mean[k])

bayreg/linear_regression.py
@@ -556,7 +556,8 @@ class ConjugateBayesianLinearRegression:
             data_scales = self.data_transformer.scale_
             sd_y, sd_x = data_scales[0], data_scales[1:]
             W = np.diag(sd_x)
-            pcc = (W @ pcc @ W) / sd_y ** 2
+            pcc = W @ pcc @ W
+            print(pcc)
 
         pcp = mat_inv(pcc)
 